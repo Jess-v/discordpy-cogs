a6f7006fcfe847cc10679d0371a9bfb99ac94f62
Jess-v <67984059+Jess-v@users.noreply.github.com>
2020-08-18 19:53:48 -0500

    added additional error checking

--- cogs/dice-roller.py
+++ cogs/dice-roller.py
@@ -11,13 +11,15 @@ class Dice(commands.Cog):
     @commands.command()
     async def roll(self, ctx: commands.Context, arg: str = '1d20'):
 
+        SYNTAX_ERR = 'Invalid command syntax. Your roll should look something like 1d20+1'
+
         roll_args = arg.split('d')
         to_add = 0
 
         random.seed(time.time() * random.random())
 
         if len(roll_args) != 2:
-            await ctx.send('Invalid command syntax. Your roll should look something like 1d20+1')
+            await ctx.send(SYNTAX_ERR)
 
         if '+' in roll_args[1] or '-' in roll_args[1]:
             to_mod = roll_args.pop(1)
@@ -26,20 +28,32 @@ class Dice(commands.Cog):
 
                 roll_args.extend(to_mod)
 
+                try:
                 to_add = int(roll_args[2])
+                except ValueError:
+                    await ctx.send(SYNTAX_ERR)
+                    return
             else:
                 to_mod = to_mod.split('-')
 
                 roll_args.extend(to_mod)
 
+                try:
                 to_add = -1 * int(roll_args[2])
+                except ValueError:
+                    await ctx.send(SYNTAX_ERR)
+                    return
 
         result = 0
         l_index = 0
 
+        try:
         while l_index < int(roll_args[0]):
             result += random.randint(1, int(roll_args[1]))
             l_index += 1
+        except ValueError:
+            await ctx.send(SYNTAX_ERR)
+            return
 
         result += to_add
 
