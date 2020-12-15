ba369d256c72890dfd0ad1b32bbcdca958f94954
Jessica V <67984059+Jess-v@users.noreply.github.com>
2020-12-15 12:52:28 -0600

    Update scraper.py
    will now iterate through every channel in every server the bot has been added to

--- cogs/scraper.py
+++ cogs/scraper.py
@@ -4,7 +4,6 @@ from discord.ext import commands
 message_log = "msg_scrape.txt"
 max_limit = 10000
 
-
 class Scrape(commands.Cog):
 
     def __init__(self, bot):
@@ -16,13 +15,20 @@ class Scrape(commands.Cog):
         if user is None:
             await ctx.send('Please tag a user who\'s messages are to be scraped.')
         else:
-            await ctx.send(f'Beginning to scrape messages of {user.display_name} in channel {ctx.channel.name}')
-            log = open(message_log, 'a', encoding='utf-8')
-            async for message in ctx.channel.history(limit=max_limit):
-                if message.author == user:
-                    log.write(f'<|startoftext|>\n{message.content}\n<|endoftext|>\n\n')
-            await ctx.send(f'Scrape complete!')
+            for guild in self.bot.guilds:
+                await ctx.send(f'Beginning to scrape messages of {user.display_name} in server {guild.name}')
+                for channel in guild.text_channels:
+                    await ctx.send(f'Beginning to scrape messages of {user.display_name} in channel {channel.name}')
+                    log = open(message_log, 'a', encoding='utf-8')
+                    try:
+                        async for message in channel.history(limit=max_limit):
+                            if message.author == user:
+                                log.write(f'<|startoftext|>\n{message.content}\n<|endoftext|>\n\n')
+                    except:
+                        await ctx.send('Permission error scraping this channel.')
+                    await ctx.send(f'Scrape complete!')
 
 
 def setup(bot):
     bot.add_cog(Scrape(bot))
+
